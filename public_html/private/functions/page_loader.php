ecae0a363399e38fa87ba34012ecb7eebbc9f0a0
wareaglez2007 <rostom.sahakian@gmail.com>
2015-09-19 19:26:55 -0700

    page_loader upodates

--- public_html/private/functions/page_loader.php
+++ public_html/private/functions/page_loader.php
@@ -5,4 +5,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+class page_loader{
+    
+    
+    public function head($title, array $js, array $css){
+        
+        $html_head="";
+        ?>
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta charset="UTF-8"> 
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          
+          <title><?= $title ?></title>
+   <?php
+      foreach ($css as $css_file){
+          echo '<link href="assets/css/'.$css_file.'" rel="stylesheet">';
+        }
+   
+      foreach ($js as $jsfile){
+        echo '<script src="assets/js/'.$jsfile.'"></script>';
+        }
+        
+        return $html_head;   
+    }
+    public function body(){
+        
+    }
+    public function footer(){
+        
+    }
+    
+    public function html_loader($title){
+        
+    }
+    
+    
+    
+    
+    
+}
